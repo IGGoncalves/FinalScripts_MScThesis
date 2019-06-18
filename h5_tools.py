caf5d81abbfb272c13d9c1ae1da582497e57f748
ines <m.ines.ggoncalves@gmail.com>
2019-06-18 19:30:03 +0100

    Added function to plot get jumps and info on them

--- h5_tools.py
+++ h5_tools.py
@@ -3,6 +3,8 @@
 ### IMPORT LIBRARIES ###
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def geth5Data(filesID, path = '', lastTimePoint=710, timestep=2):
     """
@@ -28,6 +30,7 @@ def geth5Data(filesID, path = '', lastTimePoint=710, timestep=2):
     params['kECM'] = f['results']['params']['kECM']['data'].value
     params['pFA_rev'] = f['results']['params']['pFA_rev']['data'].value
     params['lt_FA0'] = -1/(np.log(1 - f['results']['params']['pFA_rev']['data'].value))/60
+    params['lt_FA0'] = params['lt_FA0'].round(decimals=1)
 
     data = pd.DataFrame(0.0, index=range(0, 710 * 24 * 5),
                         columns=['time', 'sim_num', 'samp_num', 'kECM', 'log_kECM', 'pFA_rev', 'lt_FA0', 'nFA',
@@ -44,7 +47,7 @@ def geth5Data(filesID, path = '', lastTimePoint=710, timestep=2):
             data['pFA_rev'][m * 24 * 710 + (n * 710): m * 24 * 710 + (n * 710) + 710] = params['pFA_rev'][n]
             data['kECM'][m * 24 * 710 + (n * 710): m * 24 * 710 + (n * 710) + 710] = params['kECM'][n]
             data['log_kECM'][m * 24 * 710 + (n * 710): m * 24 * 710 + (n * 710) + 710] = np.log10(params['kECM'][n])
-            data['lt_FA0'][m * 24 * 710 + (n * 710): m * 24 * 710 + (n * 710) + 710] = params['lt_FA0'][n].round(decimals=1)
+            data['lt_FA0'][m * 24 * 710 + (n * 710): m * 24 * 710 + (n * 710) + 710] = params['lt_FA0'][n]
             data['sim_num'][m * 24 * 710 + (n * 710): m * 24 * 710 + (n * 710) + 710] = m + 1
             data['samp_num'][m * 24 * 710 + (n * 710): m * 24 * 710 + (n * 710) + 710] = n
 
@@ -68,13 +71,13 @@ def geth5Data(filesID, path = '', lastTimePoint=710, timestep=2):
 
             data['diff_disp'][m * 24 * 710 + (n * 710) + 1: m * 24 * 710 + (n * 710) + 710] = diff_disp
             data['sum_disp'][m * 24 * 710 + (n * 710) + 1: m * 24 * 710 + (n * 710) + 710] = cum_sum
-            data['abs_disp'][m * 24 * 710 + (n * 710) + 1: m * 24 * 710 + (n * 710) + 710] = cum_sum
+            data['abs_disp'][m * 24 * 710 + (n * 710) + 1: m * 24 * 710 + (n * 710) + 710] = diff_disp
 
             final_disp = np.mean(cum_sum[-5:])
 
             data['final_disp'][m * 24 * 710 + (n * 710): m * 24 * 710 + (n * 710) + 710] = final_disp
 
-    return data
+    return data, params
 
 
 def plotFinalDisp(h5Data, mode='avg'):
@@ -88,9 +91,6 @@ def plotFinalDisp(h5Data, mode='avg'):
     filesID (int/array) -
     path (string) - path to the folder containing all VTP files. If it is not specified, current dir is used.
     """
-
-    import matplotlib.pyplot as plt
-    import seaborn as sns
 
     plt.figure(figsize=(20, 10))
     sns.set_style("white")
@@ -140,3 +140,183 @@ def plotFinalDisp(h5Data, mode='avg'):
         # plt.suptitle('Mean and SEM of the final displacement of the 5 simulations', weight = 'bold')
 
 
+def plotMetric3D(metric, h5Data):
+
+    from mpl_toolkits.mplot3d import Axes3D
+    plt.rcParams['grid.color'] = "lightgray"
+    plt.rcParams['grid.linewidth'] = .5
+
+    ### 3D PLOT ###
+    fig = plt.figure(figsize=(14, 8))
+    ax = fig.gca(projection='3d')
+
+    y = h5Data['kECM'][h5Data['time'] == 0][h5Data['sim_num'] == 1]
+    x = h5Data['lt_FA0'][h5Data['time'] == 0][h5Data['sim_num'] == 1]
+
+    surf = ax.plot_surface(np.log10(x.as_matrix().reshape((6, 4))),
+                           np.log10(y.as_matrix().reshape((6, 4))),
+                           h5Data.groupby('samp_num')[metric].mean().as_matrix().reshape((6, 4)),
+                           cmap='BuGn', linewidths=0)
+
+    yticks = np.unique(h5Data['kECM'])
+    xticks = np.unique(h5Data['lt_FA0'])
+    ax.set_yticks(np.log10(yticks))
+    ax.set_yticklabels(yticks)
+
+    ax.set_xticks(np.log10(xticks))
+    ax.set_xticklabels(xticks)
+
+    ax.set_ylabel('kECM [N/m]', labelpad=20)
+    ax.set_xlabel('Lifetime [min]', labelpad=20)
+    ax.set_zlabel('Displacement [nm]', labelpad=10)
+    #plt.gca().invert_xaxis()
+    cb = fig.colorbar(surf)
+    cb.outline.set_visible(False)
+    # Get rid of the panes
+    ax.w_xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.w_yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.w_zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.w_xaxis.line.set_color('gray')
+    ax.w_yaxis.line.set_color('gray')
+    ax.w_zaxis.line.set_color('gray')
+
+
+def plotMetricHeatMap(metric, h5Data, params):
+
+    fig, ax = plt.subplots(figsize=(9, 7))
+
+    new_metric_df = h5Data.groupby('samp_num')[metric].mean()
+
+    new_metric_df = pd.merge(new_metric_df, params, on='samp_num')
+
+    # Creating the heatmaps
+    new_metric_df= new_metric_df.pivot('kECM', 'lt_FA0', metric)
+
+    # Plotting the heatmaps (saving colormap to plot colorbar afterwards)
+    im = sns.heatmap(new_metric_df, cmap="BuGn", linewidths=.9, annot=True, ax=ax)
+    plt.gca().invert_yaxis()
+    ax.set_yticklabels(np.unique(params['kECM']), va='center')
+    ax.set_ylabel('kECM [N/m]', labelpad=15)
+    ax.set_xlabel('Lifetime [min]', labelpad=15)
+    #plt.title('Number of "negative" jumps', weight='bold')
+
+
+def plotDisp(h5Data, mode='all', disp='sum_disp'):
+
+    sns.set_style("white")
+    sns.set_palette('viridis_r', 6)
+
+    if mode == 'all':
+
+        g = sns.FacetGrid(h5Data[h5Data['sim_num'] == 1], col="pFA_rev", hue="kECM", margin_titles=True, size=5)
+
+        g = (g.map(plt.plot, "time", "sum_disp")
+             .set(xlim=(0, None))
+             .add_legend()
+             .set_ylabels("Displacement [$\mu$m]", labelpad=10)
+             .set_xlabels("Time [min]", labelpad=15)
+             .fig.subplots_adjust(wspace=.1, hspace=.05))
+
+        plt.subplots_adjust(top=.8)
+        #plt.suptitle("Displacement of the cell's center of mass (simulation 1)", weight='bold')
+
+    elif mode == 'overlap':
+
+        ### PLOT DISPLACEMENT - CUMSUM (VARIATION) ###
+        g = sns.FacetGrid(h5Data, col="pFA_rev", hue="kECM", size=5)
+
+        g = (g.map(sns.lineplot, "time", disp)
+             .set(xlim=(0, None))
+             .add_legend()
+             .set_ylabels("Displacement [$\mu$m]", labelpad=10)
+             .set_xlabels("Time [min]", labelpad=15))
+
+
+def getJumps(h5Data):
+
+    from scipy.signal import find_peaks
+
+    # Set the minimum difference in displacement to be considered a jump
+    partialJumpMin = 2e-1
+    fullJumpMin = 5e-1
+
+    # Define dictionary to store the time values for the jumps of each sample (for all simulations)
+    allSimJumps = {}
+
+    # Define DataFrame to store general information on the jumps for each sample
+    jumpsInfo = pd.DataFrame(np.nan, index=range(0, 24 * 5), columns=['sim_num', 'samp_num', 'pFA_rev', 'lt_FA0', 'kECM',
+                                                                       'jumps_num', 'full_jump_num', 'partial_jump_num',
+                                                                       'first_full_jump', 'jump_time_mean',
+                                                                       'jump_time_std', 'neg_jumps_num'])
+
+    for sim in range(1, 6):
+
+        # Define dictionary to store the time values for the jumps of each sample
+        specificSimJumps = {}
+
+        # Going through the samples to store (and print) information on jumps
+        for samp in range(0, 24):
+
+            sampData = h5Data[h5Data['sim_num'] == sim][h5Data['samp_num'] == samp]
+            sampDiffDisp = sampData['diff_disp']
+
+            # Setting a threshold (minimum only, to prevent a small jump appearing next to a big one)
+            jumps, _ = find_peaks(abs(sampDiffDisp),distance=15, height=partialJumpMin)
+
+            jumpsDiffDispValues = abs(sampDiffDisp.iloc[jumps])
+
+
+            # Differentiating small and big jumps
+            partialJumps = jumps[jumpsDiffDispValues < fullJumpMin]
+            fullJumps = jumps[jumpsDiffDispValues >= fullJumpMin]
+
+            # Small adjusment because of the timestep
+            partialJumps = partialJumps * 2
+            fullJumps = fullJumps * 2
+
+            negJumps = fullJumps[sampDiffDisp.iloc[fullJumps / 2] < 0]
+
+            # Dictionary with all jumps, partial and full
+            jumpValues = {}
+
+            jumpValues['all_jumps'] = jumps * 2
+            jumpValues['partial_jumps'] = partialJumps
+            jumpValues['full_jumps'] = fullJumps
+            jumpValues['neg_jumps'] = negJumps
+
+            # Store information
+            jumpsInfo['sim_num'][(sim - 1) * 24 + samp] = sim
+            jumpsInfo['samp_num'][(sim - 1) * 24 + samp] = samp
+            jumpsInfo['jumps_num'][(sim - 1) * 24 + samp] = np.size(jumps)
+            jumpsInfo['full_jump_num'][(sim - 1) * 24 + samp] = np.size(fullJumps)
+            jumpsInfo['partial_jump_num'][(sim - 1) * 24 + samp] = np.size(partialJumps)
+            jumpsInfo['pFA_rev'][(sim - 1) * 24 + samp] = sampData['pFA_rev'][sampData['time'] == 0]
+            jumpsInfo['lt_FA0'][(sim - 1) * 24 + samp] = sampData['lt_FA0'][sampData['time'] == 0]
+            jumpsInfo['kECM'][(sim - 1) * 24 + samp] = sampData['kECM'][sampData['time'] == 0]
+
+            if np.size(jumps) == 0:
+
+                jumpsInfo['first_full_jump'][(sim - 1) * 24 + samp] = np.nan
+                jumpsInfo['jump_time_mean'][(sim - 1) * 24 + samp] = np.nan
+                jumpsInfo['jump_time_std'][(sim - 1) * 24 + samp] = np.nan
+                jumpsInfo['neg_jumps_num'][(sim - 1) * 24 + samp] = 0
+
+            else:
+
+                if np.size(fullJumps) == 0:
+                    jumpsInfo['first_full_jump'][(sim - 1) * 24 + samp] = np.nan
+                    jumpsInfo['neg_jumps_num'][(sim - 1) * 24 + samp] = 0
+
+                else:
+                    jumpsInfo['first_full_jump'][(sim - 1) * 24 + samp] = fullJumps[0]
+                    jumpsInfo['neg_jumps_num'][(sim - 1) * 24 + samp] = np.size(negJumps)
+
+                jumpsInfo['jump_time_mean'][(sim - 1) * 24 + samp] = np.mean(np.diff(jumps))
+                jumpsInfo['jump_time_std'][(sim - 1) * 24 + samp] = np.std(np.diff(jumps))
+
+            # Store the jump information in the dictionary, with the sample number as key
+            specificSimJumps[samp] = jumpValues
+
+        allSimJumps[sim] = specificSimJumps
+
+    return allSimJumps, jumpsInfo
